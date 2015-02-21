e397b44db758b4edb9e84f89fc601b87ae1c9486
seku <sekupyna@gmail.com>
2015-02-21 11:51:17 +0100

    kontakt tab

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,4 +1,7 @@
 $(document).ready(function() {
+  height = $(window).height()
+  $(".container-fluid").height(height)
+
   $(".menuItem").click(function(event) {
     event.stopPropagation()
     scrollTo = $(this).attr("data-target")
