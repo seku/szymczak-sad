10b2e15eee0673e9c89e2e8d06fa8a066ddcd229
seku <sekupyna@gmail.com>
2015-02-22 10:55:05 +0100

    country flags

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
   height = $(window).height()
-  if (height < 750) {
-    height = 750
+  if (height < 700) {
+    height = 700
   }
-
+  console.log("he", height)
   $(".container-fluid").height(height)
 
   $(".menuItem").click(function(event) {
